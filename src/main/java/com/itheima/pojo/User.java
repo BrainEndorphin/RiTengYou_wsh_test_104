6a75c84f1812441262ee507098be62b9e0ec0b97
RiTengYou <17698293667@163.com>
2022-03-28 00:05:23 +0800

    init

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,15 @@
+package com.itheima.pojo;
+
+/**
+ * @Author: RiTengYou
+ * @ProjectName: gitest
+ * @Package: com.itheima.pojo
+ * @ClassName: User
+ * @Date: 2022/3/27 21:33
+ * @Version: NULL
+ * @Description:
+ */
+public class User {
+    private String username;
+    private int age;
+}
